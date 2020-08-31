3cbe1ebe872113d67f62bcfc520c1f5ca0212991
Antonis <ant.sardounis@gmail.com>
2020-08-31 17:00:34 +0300

    added changes for episode GraphQL Service Layer with GRPC and More TypeScript Type Mapping

--- graphql/graphQLServer.ts
+++ graphql/graphQLServer.ts
@@ -5,18 +5,35 @@ import {
   makeExecutableSchema,
   ApolloServer,
 } from "apollo-server-express";
+
+// import { mergeTypes, fileLoader } from "merge-graphql-schemas";
+
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
+
 import { CustomContext } from "./context/customContext";
-import { resolvers } from "./resolvers/resolvers";
+// import { resolvers } from "./resolvers/resolvers";
+import path from 'path';
 
 const PORT = 5600;
 const app = express();
 
-const schemas: string = "type Query{p:Test} type Test { id: Int!}";
+// const schemas: string = "type Query{p:Test} type Test { id: Int!}";
+
+const resolverPath = path.join(__dirname, "./resolvers");
+const allResolvers = loadFilesSync(resolverPath);
+const mergedResolvers = mergeResolvers(allResolvers);
+
+const schemasPath = loadFilesSync(
+  path.join(__dirname, "./schemas/*.graphql")
+);
+
+const schemas = mergeTypeDefs(schemasPath);
 const services: any = {};
 const config: ApolloServerExpressConfig = {
   schema: makeExecutableSchema({
     typeDefs: schemas,
-    resolvers: resolvers,
+    resolvers: mergedResolvers,
   }),
   dataSources: () => {
     return services as any;
